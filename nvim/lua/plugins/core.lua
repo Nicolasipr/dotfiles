6462a0cce2200db24bdebacde5983015ed0bc335
nicolasipr <nicolasipr@MacBook-Pro.local>
2025-01-05 12:51:21 -0300

    (chore): Updated lazy vim configurations

---
+++ nvim/lua/plugins/core.lua
@@ -0,0 +1,11 @@
+return {
+    { "folke/lazy.nvim", version = false },
+    { "LazyVim/LazyVim", version = false, opts = {
+        colorscheme = "catppuccin",
+    } },
+    { "nvim-telescope/telescope.nvim", version = false },
+    { "nvim-lua/plenary.nvim", version = false },
+    {
+        "nvim-treesitter/nvim-treesitter", version = false
+    }
+}
