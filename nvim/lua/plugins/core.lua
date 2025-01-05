2c6b9f37656516f14f24305f0f8be0849497529e
nicolasipr <nicolasipr@MacBook-Pro.local>
2025-01-05 18:16:57 -0300

    (chore): Updated neovin configuration

--- nvim/lua/plugins/core.lua
+++ nvim/lua/plugins/core.lua
@@ -1,11 +1,28 @@
 return {
-    { "folke/lazy.nvim", version = false },
-    { "LazyVim/LazyVim", version = false, opts = {
-        colorscheme = "catppuccin",
-    } },
-    { "nvim-telescope/telescope.nvim", version = false },
-    { "nvim-lua/plenary.nvim", version = false },
+    {
+        "folke/lazy.nvim",
+        version = false
+    },
+    {
+        "LazyVim/LazyVim",
+        version = false,
+        opts = {
+            colorscheme = "catppuccin",
+        }
+    },
+    {
+        "nvim-telescope/telescope.nvim",
+        version = false
+    },
+    {
+        "nvim-lua/plenary.nvim",
+        version = false
+    },
     {
         "nvim-treesitter/nvim-treesitter", version = false
+    },
+    {
+        'ThePrimeagen/vim-be-good',
+        version = false
     }
 }
